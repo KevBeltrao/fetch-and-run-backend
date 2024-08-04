b26ff8c7d9cb506969ef76165228ce200985fcdb
Kevin Beltrao de Melo <kbmelo@outlook.com>
2024-08-03 23:06:13 -0300

    message protocol + client update + game state management

---
+++ docs/message-protocol.md
@@ -0,0 +1,10 @@
+```
+{
+    "type": "playerMove",
+    "payload": {
+        "playerID": "player1",
+        "x": 100,
+        "y": 150
+    }
+}
+```
--- internal/server/websocket/handler.go
+++ internal/server/websocket/handler.go
@@ -4,10 +4,10 @@ import (
 	"log"
 	"net/http"
 
-	"github.com/gorilla/websocket"
+	gorillaWebsocket "github.com/gorilla/websocket"
 )
 
-var upgrader = websocket.Upgrader{
+var upgrader = gorillaWebsocket.Upgrader{
 	CheckOrigin: func(request *http.Request) bool {
 		return true
 	},
@@ -18,30 +18,38 @@ func HandleConnections(
 	writer http.ResponseWriter,
 	request *http.Request,
 ) {
+	log.Println("HandleConnections called")
+
 	matchId := request.URL.Query().Get("matchId")
 	if matchId == "" {
 		log.Println("Match ID is required")
 		return
 	}
 
+	log.Println("Match ID:", matchId)
+
 	websocket, err := upgrader.Upgrade(writer, request, nil)
 	if err != nil {
 		log.Println("Error upgrading connection", err)
+		return
 	}
 	defer websocket.Close()
 
-	hub := manager.GetHub(matchId)
+	log.Println("WebSocket connection upgraded")
 
 	client := &Client{
-		hub:        hub,
 		connection: websocket,
 		send:       make(chan []byte, 256),
 	}
 
 	manager.JoinHub(matchId, client)
+	client.hub = manager.GetHub(matchId)
+
+	log.Println("Client joined hub")
 
 	go client.writePump()
 	client.readPump()
 
+	log.Println("Client left hub")
 	manager.LeaveHub(matchId, client)
 }
